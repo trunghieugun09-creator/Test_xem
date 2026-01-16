da22b4ae2d6664d00d877838054ef02e6d35f048
nuxz <trunghieugun09@gmail.com>
2026-01-16 12:27:08 +0700

    Add files via upload

---
+++ keep_alive (1).py
@@ -0,0 +1,18 @@
+from flask import Flask
+from threading import Thread
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot is alive!"
+
+def run():
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.daemon = True
+    t.start()
